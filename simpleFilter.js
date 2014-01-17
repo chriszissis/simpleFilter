0a394a6ad0f08298b54d6357f9ea487281747efc
Christopher Zissis <cz113@nova.edu>
2014-01-17 15:01:58 -0500

    fixed for multiple tables

--- simpleFilter.js
+++ simpleFilter.js
@@ -9,22 +9,24 @@
 			exclusions: []
 		}, options);		
 		
-		this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" id="filterBy" name="filterBy" /></div><br style="clear:both;"/>');
+		this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" /></div><br style="clear:both;"/>');
 		
-		$(document).on('keyup', '#filterBy', function(){
-			checkRows($(this).val());
+		$(document).on('keyup', '.filterBy', function(){
+			checkRows($(this).val(), $(this).parent('div').siblings('table'));
 		});
 		
-			function checkRows(filterText){
-				$('.filter > tbody > tr').each(function(i,v){
-					if ( ! ( $(v).text().toUpperCase().match(filterText.toUpperCase())) )
-					{
-						$(v).hide();
-					}	
-					else {
-						$(v).show();
-					}					
-				});
+			function checkRows(filterText,el){
+				
+					$('tbody > tr', el).each(function(i,v){
+						if ( ! ( $(v).text().toUpperCase().match(filterText.toUpperCase())) )
+						{
+							$(v).hide();
+						}	
+						else {
+							$(v).show();
+						}					
+					});
+				
 			}
 		
 		return this;
