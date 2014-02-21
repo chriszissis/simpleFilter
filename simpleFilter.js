9d7f3171e24eb59ff05a52634d86efc05dec8bca
Christopher Zissis <cz113@nova.edu>
2014-02-21 14:35:56 -0500

    fixed issue when using multiple tables

--- simpleFilter.js
+++ simpleFilter.js
@@ -6,21 +6,22 @@
 		
 		var settings = $.extend({
 			autoPlacement: true,
-			manualPlacementDiv: '.filterContainer',
 			fieldAlign: 'right',
 			exclusions: []
 		}, options);		
 		
-		var rndID = Math.floor(Math.random() * 1000000);
-		$(this).attr('data-sf', rndID );
+		$(table).each(function(){
+			var rndID = Math.floor(Math.random() * 1000000);
+			$(this).attr('data-sf', rndID );
+			
+			if (settings.autoPlacement) {
+				$(this).before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/></div><br style="clear:both;"/>');
+			}
+			else {
+				$(this).parents('.filter_wrapper').find('.filterContainer').append('<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/>');
+			}
+		});
 		
-		if (settings.autoPlacement) {
-			this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/></div><br style="clear:both;"/>');
-		}
-		else {
-			$(settings.manualPlacementDiv).append('<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/>');
-		}
-	
 		$(document).on('keyup', '.filterBy', function(){
 			var t = $(this).attr('data-sfT');
 			checkRows($(this).val(), $('table[data-sf=' + t + ']'));
