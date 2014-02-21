57aed2f72bc0a8a5ae13e17f095bb7cf29f282d5
Christopher Zissis <cz113@nova.edu>
2014-02-21 14:13:56 -0500

    switched DOM relative search for table to random ids

--- simpleFilter.js
+++ simpleFilter.js
@@ -11,15 +11,19 @@
 			exclusions: []
 		}, options);		
 		
+		var rndID = Math.floor(Math.random() * 1000000);
+		$(this).attr('data-sf', rndID );
+		
 		if (settings.autoPlacement) {
-			this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" /></div><br style="clear:both;"/>');
+			this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/></div><br style="clear:both;"/>');
 		}
 		else {
-			$(settings.manualPlacementDiv).append('<input type="text" class="filterBy" />');
+			$(settings.manualPlacementDiv).append('<input type="text" class="filterBy" placeholder="Filter" data-sfT="' + rndID + '"/>');
 		}
 	
 		$(document).on('keyup', '.filterBy', function(){
-			checkRows($(this).val(), $(this).parent('div').siblings('table'));
+			var t = $(this).attr('data-sfT');
+			checkRows($(this).val(), $('table[data-sf=' + t + ']'));
 		});
 		
 		function checkRows(filterText,el){
