8e6c086fcec53e5b9ac87fbf67d640387ab27a1a
Christopher Zissis <cz113@nova.edu>
2014-02-21 12:57:32 -0500

    added ability to manually position filter box

--- simpleFilter.js
+++ simpleFilter.js
@@ -5,29 +5,35 @@
 		var table = this;
 		
 		var settings = $.extend({
+			autoPlacement: true,
+			manualPlacementDiv: '.filterContainer',
 			fieldAlign: 'right',
 			exclusions: []
 		}, options);		
 		
-		this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" /></div><br style="clear:both;"/>');
-		
+		if (settings.autoPlacement) {
+			this.before('<div class="filterContainer" style="float: ' + settings.fieldAlign + ';"><span class="filterLabel">Filter By:</span>&nbsp;<input type="text" class="filterBy" /></div><br style="clear:both;"/>');
+		}
+		else {
+			$(settings.manualPlacementDiv).append('<input type="text" class="filterBy" />');
+		}
+	
 		$(document).on('keyup', '.filterBy', function(){
 			checkRows($(this).val(), $(this).parent('div').siblings('table'));
 		});
 		
-			function checkRows(filterText,el){
-				
-					$('tbody > tr', el).each(function(i,v){
-						if ( ! ( $(v).text().toUpperCase().match(filterText.toUpperCase())) )
-						{
-							$(v).hide();
-						}	
-						else {
-							$(v).show();
-						}					
-					});
-				
-			}
+		function checkRows(filterText,el){
+			
+			$('tbody > tr', el).each(function(i,v){
+				if ( ! ( $(v).text().toUpperCase().match(filterText.toUpperCase())) )
+				{
+					$(v).hide();
+				}	
+				else {
+					$(v).show();
+				}					
+			});
+		}
 		
 		return this;
 	};
